eeba6c8ad9250c4718d637cd0d8a6077b4ab9f2e
MatthewPaulGithub <mpaul@cityassets.com>
2019-01-30 20:47:28 -0500

    Add OpenPopClose

--- assets/javascripts/redmine_im_link.js
+++ assets/javascripts/redmine_im_link.js
@@ -25,3 +25,24 @@ function OpenTab(url) {
         newWindow.focus();
     }
 }
+
+
+function OpenPopClose(url, w, h) {
+    // Fixes dual-screen position                         Most browsers      Firefox
+    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+    var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+    var top = ((height / 2) - (h / 2)) + dualScreenTop;
+    var newWindow = window.open(url, '_blank', 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+    // Puts focus on the newWindow
+    // if (window.focus) {
+    //     newWindow.focus();
+    // }
+	
+	setTimeout(function(){ newWindow.close() }, 5000);
+}
